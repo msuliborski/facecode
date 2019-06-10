80069387ed3984440a851cba970a5613fd6642a7
Michał Suliborski <msuliborski@gmail.com>
2019-06-10 13:19:09 +0200

    layout and function update

---
+++ app/src/main/java/com/facecoders/facecode/activities/MenuActivity.java
@@ -0,0 +1,34 @@
+package com.facecoders.facecode.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+
+import com.facecoders.facecode.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    ImageButton photoImageButton;
+    ImageButton cameraImageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        photoImageButton = findViewById(R.id.photoImageButton);
+        cameraImageButton = findViewById(R.id.cameraImageButton);
+
+        photoImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, PhotoActivity.class);
+            startActivity(intent);
+        });
+
+        cameraImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, CameraActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
