83f38de40191c6c4fecc4784f5bccc8719d56656
Michał Suliborski <msuliborski@gmail.com>
2019-06-13 18:16:06 +0200

    camera2 working

--- app/src/main/java/com/facecoders/facecode/activities/WelcomeActivity.java
+++ app/src/main/java/com/facecoders/facecode/activities/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.facecoders.facecode.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.facecoders.facecode.R;
@@ -10,6 +11,7 @@ import com.facecoders.facecode.R;
 public class WelcomeActivity extends AppCompatActivity {
 
     ImageButton candidImageButton;
+    Button tempButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,11 @@ public class WelcomeActivity extends AppCompatActivity {
         candidImageButton = findViewById(R.id.candidImageButton);
         candidImageButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        });
+        tempButton = findViewById(R.id.tempButton);
+        tempButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Camera2Activity.class);
             startActivity(intent);
         });
 
