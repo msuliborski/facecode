dde0e6bee22ca0bb666dfc1b55f4ff2ef3d47275
Michał Suliborski <msuliborski@gmail.com>
2019-06-15 13:34:57 +0200

    everything working, beta release

--- app/src/main/java/com/facecoders/facecode/activities/PhotoActivity.java
+++ app/src/main/java/com/facecoders/facecode/activities/PhotoActivity.java
@@ -1,68 +1,57 @@
 package com.facecoders.facecode.activities;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
 
 import com.facecoders.facecode.R;
-import com.facecoders.facecode.camera.CameraHandler;
-import com.facecoders.facecode.camera.CameraPreview;
-import com.facecoders.facecode.tflite.Classifier;
-import com.facecoders.facecode.tflite.ClassifierHandler;
+import com.facecoders.facecode.camera.Camera2Fragment;
 
-import java.util.List;
+import java.nio.ByteBuffer;
 
 
 public class PhotoActivity extends AppCompatActivity {
 
-    private Camera camera;
 
-    FrameLayout cameraPreviewFrameLayout;
-    ImageButton takePhotoImageButton;
-    boolean cameraFacingFront = true;
+    boolean cameraFacingFront = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
 
-        cameraPreviewFrameLayout = findViewById(R.id.cameraPreviewFrameLayout);
-        takePhotoImageButton = findViewById(R.id.takePhotoImageButton);
-        takePhotoImageButton.setOnClickListener(v -> {
-            camera.takePicture(null, null, mPicture);
+        Camera2Fragment camera2BasicFragment = Camera2Fragment.newInstance(cameraFacingFront, false);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.cameraPreviewFrameLayout, camera2BasicFragment)
+                    .commit();
+        }
+
+
+        Intent intent = new Intent(this, PhotoResultActivity.class);
+        camera2BasicFragment.mOnImageAvailableListener = reader -> {
+            Image mImage = reader.acquireNextImage();
+            ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.remaining()];
+            buffer.get(bytes);
+            mImage.close();
+
+            intent.putExtra("bytes", bytes);
+            intent.putExtra("cameraFacingFront", cameraFacingFront);
+            startActivity(intent);
+        };
+
+        findViewById(R.id.pickFromGalleryImageButton).setOnClickListener(v -> {
         });
 
-//        camera = CameraHandler.getCameraInstance(cameraFacingFront);
-//        CameraHandler.setParameters();
-//        v.addView(new CameraPreview(this, camera));
-//        cameraPreview.
-    }
+        findViewById(R.id.takePhotoImageButton).setOnClickListener(v -> {
+            camera2BasicFragment.takePicture();
+        });
 
-    private Camera.PictureCallback mPicture = (bytes, camera) -> {
-        cameraPreviewFrameLayout.removeAllViews();
-        camera.stopPreview();
-        Intent intent = new Intent(this, PhotoResultActivity.class);
-        intent.putExtra("bytes", bytes);
-        startActivity(intent);
-    };
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        cameraPreviewFrameLayout.removeAllViews();
-        camera.release();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        camera = CameraHandler.getCameraInstance(cameraFacingFront);
-        CameraHandler.setParameters();
-        cameraPreviewFrameLayout.addView(new CameraPreview(this, camera));
+        findViewById(R.id.switchCameraImageButton).setOnClickListener(v -> {
+            cameraFacingFront = !cameraFacingFront;
+            camera2BasicFragment.switchCamera(cameraFacingFront, false);
+        });
     }
 }
